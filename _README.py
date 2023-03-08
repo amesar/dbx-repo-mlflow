9aaf14326f873b1b92979b740b2e8e37dbc70de2
amesar <amesar>
2023-03-08 12:56:48 +0000

    Updated main _README

--- _README.py
+++ _README.py
@@ -2,6 +2,9 @@
 # MAGIC %md ## _README - dbx-repo-mlflow
 # MAGIC 
 # MAGIC **Notebooks**
-# MAGIC * [01_Iris_Train]($01_Iris_Train)
+# MAGIC * [sklearn_iris]($sklearn_iris/_README)
+# MAGIC   * [01_Iris_Train]($sklearn_iris/01_Iris_Train)
+# MAGIC * [sklearn_wine]($sklearn_wine/_README)
+# MAGIC 
 # MAGIC 
 # MAGIC Last updated: 2023-03-08
