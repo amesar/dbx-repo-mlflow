450051a0305965eea871476b02f4e0c58124ffb1
amesar <amesar>
2023-03-08 12:54:18 +0000

    Create sklearn_iris and sklearn_wine folders

---
+++ sklearn_iris/_README.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+# MAGIC %md ## _README - sklearn-iris
+# MAGIC 
+# MAGIC **Notebooks**
+# MAGIC * [01_Iris_Train]($01_Iris_Train)
+# MAGIC 
+# MAGIC Last updated: 2023-03-08
---
+++ sklearn_wine/_README.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+# MAGIC %md ## _README - sklearn-wine
+# MAGIC 
+# MAGIC Last updated: 2023-03-08
